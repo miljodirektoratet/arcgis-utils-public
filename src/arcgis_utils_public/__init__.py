40c698d32d10371be10b3c0f74248dab238fd1b1
Willeke A'Campo <willeke.acampo@miljodir.no>
2026-05-12 17:20:03 +0200

    refactor: rename module

--- src/arcgis_utils_public/__init__.py
+++ src/arcgis_utils_public/__init__.py
@@ -5,10 +5,10 @@ from __future__ import annotations
 from importlib.metadata import PackageNotFoundError, version
 
 from .hello import main
-from .agol_add_users_to_group import agol_add_users_to_group
+from .agol_user_admin import agol_add_users_to_group
 try:
 	__version__ = version("arcgis-utils-public")
 except PackageNotFoundError:
 	__version__ = "unknown"
 
-__all__ = ["main", "agol_add_users_to_group", "__version__"]
+__all__ = ["main", "agol_user_admin", "__version__"]
